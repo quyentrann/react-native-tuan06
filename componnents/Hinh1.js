414a00b54c5459c1c37b02d736e918d90f0298b2
quyentrann <dangthiquyentran22@gmail.com>
2023-10-19 16:38:04 +0700

    push bai thuc hanh tuan 6

---
+++ componnents/Hinh1.js
@@ -0,0 +1,78 @@
+import React, {useState} from "react";
+import {View, Text, TouchableOpacity, StyleSheet,Image} from 'react-native'
+import { useNavigation, useRoute } from '@react-navigation/native';
+
+export default function Hinh1({route,navigation}){
+    // console.log(navigation);
+    // console.log(route);
+    // var [img,setImage] = React.useState(route.params)
+    return(
+        <View style={styles.container}>
+            <View style={styles.containerHeader}>
+
+                 <Image
+                    style={{height:135, width:110 }}
+                     source={require("../img/blue.jpg")}
+                />
+                <View style={styles.viewInfo}>
+                    <Text style={{fontWeight:"600",fontSize:19, width:200}}>Điệm thoại Vsmart Joy 3 Hàng chính hãng</Text>
+                    {/* <Text style={{fontWeight:"600",fontSize:15}} >Màu: {img}</Text> */}
+                    <View style={{fontWeight:"600",flexDirection:"row"}}>
+                        <Text style={{fontWeight:"600",fontSize:15}}>Cũng cấp bởi: </Text>
+                        <Text style={{fontWeight:"bold", fontSize:15}}>Tiki Tradding</Text>
+                    </View>
+                    <Text style={{fontWeight:"bold", fontSize:23}}>1.790.000 đ</Text>
+                </View>
+            </View>
+            <View style={styles.containerBody}>
+                <Text style={{margin:10,fontSize:20, fontWeight:"500", width:"100%"}}>Chọn một màu bên dưới:</Text>
+                <View style={{alignItems:"center"}}>
+                    <TouchableOpacity style={{height:90,width:90,marginBottom:10, backgroundColor:"#C5F1FB"}} onPress={()=> {setImage("white")}} ></TouchableOpacity>
+                    <TouchableOpacity style={{height:90,width:90,marginBottom:10, backgroundColor:"red"}} onPress={()=> {setImage("red")}} ></TouchableOpacity>
+                    <TouchableOpacity style={{height:90,width:90,marginBottom:10, backgroundColor:"black"}} onPress={()=> {setImage("black")}} ></TouchableOpacity>
+                    <TouchableOpacity style={{height:90,width:90,marginBottom:10, backgroundColor:"blue"}} onPress={()=> {setImage("blue")}} ></TouchableOpacity>
+                </View>
+                <TouchableOpacity
+                    style={{height:50,
+                            width:"80%",
+                            backgroundColor:"#4D6DC1",
+                            borderRadius:10,
+                            justifyContent:"center",
+                            marginTop:20
+                        }}
+                        onPress={()=>{ navigation.navigate('firstScreen',img)}}
+                >
+                    <Text style={{fontSize:25, fontWeight:"bold", textAlign:"center", color:"white"}}>Xong</Text>
+                </TouchableOpacity>
+            </View>
+            
+        </View>
+    );
+}
+
+let styles = StyleSheet.create({
+    container:{
+        flex:1,
+
+    },containerHeader:{
+        flex:2,
+        flexDirection:"row",
+        alignItems:"center",
+        padding:5,
+        marginLeft:10
+        
+    },
+    containerBody:{
+        flex:6,
+        marginTop:10,
+        backgroundColor:"#C4C4C4",
+        alignItems:"center"
+    },
+    viewInfo:{
+        marginLeft:10,
+        height:"100%",
+        justifyContent:"space-between"
+
+    }
+
+})
---
+++ componnents/Screen1.js
@@ -0,0 +1,2 @@
+import React from "react";
+import { Image, Text, View, TouchableOpacity, StyleSheet } from "react-native";
